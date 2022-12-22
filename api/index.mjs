5f67f36e83d1504c90b8cd9d1b7c95f5a71f25e4
Designated-Hitter <dosadola@gmail.com>
2022-12-23 00:01:13 +0900

    backend 용 npm 패키지 설치

--- api/index.mjs
+++ api/index.mjs
@@ -6,16 +6,25 @@ export const handler = async(event) => {
   const bodyString = event.body;
   const body = bodyString ? JSON.parse(bodyString) : null;
 
+  const mysql = require ('mysql2');
+  const bcrypt = require ('bcrypt');
+  const jwt = require('jsonwebtoken');
+  const code = requre('randexp');
+  
+
+
   // Root
   if (method === 'GET' && path === '/') {
     return {
       statusCode: 200,
-      body: 'Hello World!'
+      body: 'api backend test'
     }
   }
 
   if (method === 'POST' && path === '/register') {
     return {
+
+
       statusCode: 200,
       body: `${body.id}님의 회원가입을 축하합니다!`
     }
