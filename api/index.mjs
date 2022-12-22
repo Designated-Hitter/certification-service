346c90ef32b2849efc4a0aee38deb0df748f33dc
Designated-Hitter <dosadola@gmail.com>
2022-12-23 00:50:16 +0900

    회원가입 관련 mysql pool 작성(미테스트)

--- api/index.mjs
+++ api/index.mjs
@@ -10,8 +10,19 @@ export const handler = async(event) => {
   const bcrypt = require ('bcrypt');
   const jwt = require('jsonwebtoken');
   const code = requre('randexp');
-  
 
+  const pool = mysql.createPool({
+    host: 'db-private-cert-jeong.cluster-cqdqncbrwk60.ap-northeast-2.rds.amazonaws.com',
+    user: 'admin',
+    database: 'db-private-cert-jeong',
+    password: 'qBQTcReq56vm7SqV2BkQ',
+    port: '3306',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  })
+
+  const connection = pool.promise();
 
   // Root
   if (method === 'GET' && path === '/') {
@@ -21,12 +32,37 @@ export const handler = async(event) => {
     }
   }
 
+  //회원가입
   if (method === 'POST' && path === '/register') {
-    return {
+    const email = body.email;
+    const nickname = body.nickname;
+    //중복 가입 체크
+    const [rowsEmailCheck] = await connection.execute(`SELECT email FROM membership-list WHERE email = ?`, [email]);
 
+    if (rowsEmailCheck.length) {
+      res.json({
+        success: false,
+        error: '이미 가입된 이메일 입니다.'
+        });
+      return;
+    };
+    //중복 닉네임 체크
+    const [rowsNicknameCheck] = await connection.execute(`SELECT nickname FROM membership-list WHERE nickname = ?`, [nickname]);
+
+    if (rowsNicknameCheck.length) {
+      res.json({
+        success: false,
+        error: '중복된 닉네임은 사용할 수 없습니다.'
+      });
+      return;
+    };
+
+    return {
+      
+      
 
       statusCode: 200,
-      body: `${body.id}님의 회원가입을 축하합니다!`
+      body: `${nickname}님의 회원가입을 축하합니다!`
     }
   }
 };
