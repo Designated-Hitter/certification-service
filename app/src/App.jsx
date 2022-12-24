43bebe76ef8f1cc955d6a1fe9d2b12f9d1c0d5ae
Designated-Hitter <dosadola@gmail.com>
2022-12-25 01:03:24 +0900

    로그인 프론트엔드 개발시작

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,14 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
+
+import Header from './pages/Header.js';
+// import Login from './pages/Login.js';
+
+
+import Footer from './pages/Footer.js';
+
+
 import './App.css'
 
 function App() {
@@ -7,26 +16,12 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is ddididididi {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <Header />
+      {/* <Login /> */}
+
+      <Footer />
+
     </div>
   )
 }
---
+++ app/src/pages/Login.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+
+
+function Login () {
+
+  async function tryJoin () {
+    Navigate('./join.html');
+  }
+
+  async function tryLogin() {
+    const {data} = { //axios가 아닌 api gateway로 보내는 방법 조사하기
+      method: "POST",
+      url: "",
+      data: {
+        email: email
+      }
+    }
+  }
+
+  return (
+    <div className="Login">
+      <div className="email-box">
+        <input type="email" name="email" placeholder="E-mail" value={ email }/><br />
+      </div>
+      <div className="button-area">
+        <button className="join" onClick={tryJoin()}>JOIN</button>
+        <button className="login" onClick={tryLogin()}>LOGIN</button>
+      </div>
+    </div>
+  ) 
+  
+}
+
+export default Login;
