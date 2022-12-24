e97ee0eb7af335adca234132260158667df5457d
Designated-Hitter <dosadola@gmail.com>
2022-12-24 23:21:23 +0900

    프론트엔드 작성 시작

---
+++ app/src/pages/Footer.js
@@ -0,0 +1,11 @@
+
+function Footer () {
+
+  return (
+    <div className="Footer">
+      <label> Jeong Donghwan personal homework</label>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ app/src/pages/Header.js
@@ -0,0 +1,11 @@
+
+function Header () {
+
+  return (
+    <div className = "Header">
+      <label>Certification System</label>
+    </div>
+  );
+}
+
+export default Header;
