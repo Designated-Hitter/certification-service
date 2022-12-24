850eaf3d444530e1d183470d9f166822229f1f41
Designated-Hitter <dosadola@gmail.com>
2022-12-24 21:28:44 +0900

    로그인

--- api/index.mjs
+++ api/index.mjs
@@ -63,9 +63,9 @@ export const handler = async(event) => {
     const [rowsSuccessfullyJoined] = await connection.execute(`INSERT INTO membership-list(email, nickname) VALUES (?, ?)`, [email, nickname]);
     
     //인증 이메일 보내기
-    const randomCode = new RandExp(/[a-z0-9]{32}/).gen();
+    const randomCode = '';
 
-    const [rowsVerification] = await connection.execute(`INSERT INTO verification_data(email, verification_code) VALUES (?, ?)`, [email, randomCode])
+    const [rowsVerification] = await connection.execute(`INSERT INTO verification-data(email, verification_code) VALUES (?, ?)`, [email, randomCode])
 
     let emailForm = new FormData()
     emailForm.append("from", `no-reply@mail.okayu.xyz`)
@@ -79,4 +79,52 @@ export const handler = async(event) => {
       body: `${nickname}님의 회원가입을 축하합니다! 인증메일을 확인해주세요.`
     }
   }
+
+  //인증메일 체크
+
+
+  //로그인하기
+  if (method === 'POST' && path === '/') {
+    const email = body.email;
+    const hashedMail = bcrypt.hashSync(email, 10);
+    let now = new Date();
+    const hashedDate = bcyrpt.hashSync(now, 10);
+    const loginCode = bcrypt.hashSync(hashedMail + hashedDate, 10);
+    const [rowsLoginCode] = await connection.execute(`INSERT INTO login-data(email, login_code) VALUES (?, ?)`, [email, loginCode]);
+
+    let emailForm = new FormData()
+    emailForm.append("from", `no-reply@mail.okayu.xyz`)
+    emailForm.append("to", email)
+    emailForm.append("subject", `이메일 인증 코드입니다.`)
+    emailForm.append("html", `<a href="https://critique.okayu.xyz/emailVerify.html?code=${loginCode}">여기를 클릭해주세요.</a>`) //URL 수정해라
+
+    return {
+      statusCode: 200,
+      success: true,
+      body: `로그인을 완료 하려면 메일을 확인해주세요`
+    }
+  }
+
+  //로그인 메일 확인
+  if (method === 'GET' && path === '/main') {
+    const email = body.email;
+    const loginCode = body.loginCode; //수정필요
+
+    const [rowsCompare] = await connection.execute(`SELECT email, login_code FROM login-data WHERE email = ?`, [email]);
+
+    if (!rowsCompare.length) {
+      return {
+        success: false,
+        body: '잘못된 접근입니다.'
+      }
+    }
+
+    return {
+      statusCode: 200,
+      success: true,
+      body: '로그인 성공'
+    }
+
+  }
+
 };
